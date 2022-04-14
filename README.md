cd5832f9dd9938f87bfd9577f960c57b6f716de7
Nicolas <n.riquelmebareiro@gmail.com>
2022-04-14 09:47:23 +0200

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# paas-verdaccio
-paas verdaccio service
+# About
+
+## Verdaccio kuzzle
+
+This is the official Kuzzle verdaccio authentication plugin.
+
+This plugins allows you to connect to [verdaccio](https://verdaccio.org/) packages proxy registry using kuzzle users.
+
+## Kuzzle
+
+Kuzzle is an open-source backend that includes a scalable server, a multiprotocol API,
+an administration console and a set of plugins that provide advanced functionalities like real-time pub/sub, blazing fast search and geofencing.
+
+* :octocat: __[Github](https://github.com/kuzzleio/kuzzle)__
+* :earth_africa: __[Website](https://kuzzle.io)__
+* :books: __[Documentation](https://docs.kuzzle.io)__
+
+## Compatibility matrix
+
+| Kuzzle Version | Plugin Version |
+| -------------- | -------------- |
+| >= 2           | 1.x.x          |
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { Callback, Logger, IPluginAuth } from "@verdaccio/types";
+import { Kuzzle, WebSocket } from "kuzzle-sdk";
+
+interface IKuzzleAuthConfig {
+  url: string;
+}
+
+export default class KuzzleAuth implements IPluginAuth<IKuzzleAuthConfig> {
+  private logger: Logger;
+  private config: IKuzzleAuthConfig;
+
+  constructor(logger: Logger, config: IKuzzleAuthConfig) {
+    this.logger = logger;
+    this.config = config;
+  }
+
+  authenticate(user: string, password: string, cb: Callback): void {
+    const { url } = this.config;
+    const kuzzle = new Kuzzle(new WebSocket(url));
+
+    kuzzle
+      .login("local", { username: user, password })
+      .then(() => {
+        const groups: string[] = [];
+        cb(null, groups);
+      })
+      .catch((err: any) => {
+        this.logger.error(err);
+        cb(null, false);
+      })
+      .finally(() => {
+        kuzzle.disconnect();
+      });
+  }
+}
