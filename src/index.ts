7b866aab27d42bc40f3fc049728b546d66b86583
Nicolas <n.riquelmebareiro@gmail.com>
2022-04-14 14:25:50 +0200

    Bump to 1.0.7

--- src/index.ts
+++ src/index.ts
@@ -21,25 +21,26 @@ export default class KuzzleAuth implements IPluginAuth<IKuzzleAuthConfig> {
 
   authenticate(user: string, password: string, cb: Callback): void {
     const { url, port } = this.config;
-    this.logger.info(`KuzzleAuth authenticate ${user} with ${url}:${port}`);
+    this.logger.info(`KuzzleAuth authenticate ${user}`);
     const kuzzle = new Kuzzle(new WebSocket(url, { port }));
 
     kuzzle
       .connect()
       .then(() => {
+        this.logger.info(`KuzzleAuth connected to ${url}:${port}`);
         kuzzle.auth
           .login("local", { username: user, password })
-          .then(() => {
+          .then((response) => {
+            this.logger.info(`KuzzleAuth login success for ${user} with reponse ${JSON.stringify(response)}`);
             const groups: string[] = [];
+            kuzzle.disconnect();
             cb(null, groups);
           })
           .catch((err: any) => {
             this.logger.error(err);
+            kuzzle.disconnect();
             cb(null, false);
           })
-          .finally(() => {
-            kuzzle.disconnect();
-          });
       })
       .catch((err: any) => {
         this.logger.error(err);
