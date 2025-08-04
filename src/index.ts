412592b4e60b90ae2de767a97064ecc7f57330ef
rolljee <n.riquelmebareiro@gmail.com>
2025-08-04 14:20:15 +0200

    feat: update to verdaccio v6

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { AuthCallback, Logger, IPluginAuth } from "@verdaccio/types";
 import { Kuzzle, Http } from "kuzzle-sdk";
+import { User } from "kuzzle-sdk/src/core/security/User";
 
 interface IKuzzleAuthConfig {
   url: string;
@@ -19,7 +20,61 @@ export default class KuzzleAuth implements IPluginAuth<IKuzzleAuthConfig> {
     );
   }
 
-  authenticate(user: string, password: string, cb: AuthCallback): void {
+  private async loginUser(kuzzle: Kuzzle, username: string, password: string) {
+    try {
+      await kuzzle.auth.login("local", {
+        username,
+        password,
+      });
+    } catch (error: any) {
+      throw new Error(
+        `KuzzleAuth login failed for user ${username}: ${error.message}`
+      );
+    }
+  }
+
+  private async processCurrentUser(user: User) {
+    try {
+      if (!user) {
+        throw new Error("No current user found");
+      }
+
+      if (!user._source) {
+        throw new Error("No user source found");
+      }
+
+      if (!user._source.profileIds) {
+        throw new Error("No profile IDs found for the user");
+      }
+
+      const profileIds = user._source.profileIds;
+
+      if (!profileIds || profileIds.length === 0) {
+        throw new Error("User has no profiles");
+      }
+
+      this.logger.info(
+        `KuzzleAuth user ${user} has profiles: ${profileIds.join(", ")}`
+      );
+
+      if (!profileIds.includes("license-user")) {
+        throw new Error(
+          `User ${user} is not authorized with license-user profile`
+        );
+      }
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  private async logOutFromKuzzle(kuzzle: Kuzzle) {
+    await kuzzle.auth.logout();
+    kuzzle.disconnect();
+  }
+
+  public authenticate(user: string, password: string, cb: AuthCallback): void {
     const { url, port } = this.config;
     this.logger.info(`KuzzleAuth authenticate ${user}`);
     const kuzzle = new Kuzzle(new Http(url, { port }));
@@ -27,59 +82,24 @@ export default class KuzzleAuth implements IPluginAuth<IKuzzleAuthConfig> {
     kuzzle
       .connect()
       .then(() => {
-        this.logger.info(`KuzzleAuth connected to ${url}:${port}`);
-        kuzzle.auth
-          .login("local", { username: user, password })
-          .then((response) => {
-            this.logger.info(
-              `KuzzleAuth login success for ${user} with reponse ${JSON.stringify(
-                response
-              )}`
-            );
-
+        this.loginUser(kuzzle, user, password)
+          .then(() => {
             return kuzzle.auth.getCurrentUser();
           })
           .then((currentUser) => {
-            this.logger.info(
-              `KuzzleAuth current user retrieved: ${JSON.stringify(currentUser)}`
-            );
-
-            if (currentUser && currentUser._source.profileIds) {
-              const profileIds = currentUser._source.profileIds;
-              this.logger.info(
-                `KuzzleAuth user ${user} has profiles: ${profileIds.join(", ")}`
-              );
-
-              if (profileIds.includes("license-user")) {
-                this.logger.info(
-                  `KuzzleAuth user ${user} is authorized with license-user profile`
-                );
-                cb(null, ["$authenticated"]);
-              }
-            }
-
-            this.logger.info(`KuzzleAuth user ${user} is not authorized`);
-            cb(null, false);
-          })
-          .catch((err: any) => {
-            this.logger.error(err);
-            cb(null, false);
-          })
-          .finally(() => {
-            kuzzle.auth.logout()
-              .then(() => {
-                this.logger.info(`KuzzleAuth logged out user ${user}`);
-                kuzzle.disconnect();
-              })
-              .catch((logoutErr: any) => {
-                this.logger.error(`KuzzleAuth logout error: ${logoutErr}`);
-                kuzzle.disconnect();
+            this.processCurrentUser(currentUser).then((isAuthorized) => {
+              this.logOutFromKuzzle(kuzzle).then(() => {
+                if (isAuthorized) {
+                  cb(null, ["$authenticated"]);
+                } else {
+                  cb(null, false);
+                }
               });
+            });
           });
       })
       .catch((err: any) => {
         this.logger.error(err);
-        kuzzle.disconnect();
         cb(null, false);
       });
   }
